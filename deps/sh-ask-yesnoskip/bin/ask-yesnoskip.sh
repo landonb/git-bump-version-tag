ad3478635e8c10fdad1f7a99f6fdd90679ce62ef
Landon Bouma <landonb@users.noreply.github.com>
2023-11-04 18:38:15 -0500

    Deps: Update faithfuls
    - Source: sh-ask-yesnoskip @ ce1003b62f8eadf4ef2f3a2edf4c80667d1923ab
    
    - Commit generated by:
    
        https://github.com/thegittinsgood/git-update-faithful#⛲

--- deps/sh-ask-yesnoskip/bin/ask-yesnoskip.sh
+++ deps/sh-ask-yesnoskip/bin/ask-yesnoskip.sh
@@ -12,6 +12,10 @@
 ask_yesnoskip () {
   local the_ask="$1"
   local default_choice="${2:-N}"
+  local audit_choice="$3"
+
+  # ***
+
   if [ "${default_choice}" = "Y" ]; then
     lng_opts='Yes/no/skip'
     sht_opts='Y/n/s'
@@ -23,23 +27,44 @@ ask_yesnoskip () {
     sht_opts='y/n/S'
   else
     >&2 echo "ASSERT: Unrecognized default_choice: ${default_choice}"
+
     return 1
   fi
 
   ${SKIP_PROMPT_NL:-false} || >&2 echo
   >&2 printf %s "Please ${lng_opts}: ${the_ask}? [${sht_opts}] "
 
+  # ***
+
   local the_choice
-  ${SKIP_PROMPTS:-false} && the_choice='s' || read -e the_choice
-  [ -z "${the_choice}" ] && the_choice="${default_choice}"
-  the_choice="${the_choice,,}"  # lower.
+
+  if [ -z "${audit_choice}" ]; then
+    ${SKIP_PROMPTS:-false} \
+      && the_choice='s' \
+      || read -e the_choice
+  else
+    the_choice="${audit_choice}"
+  fi
+
+  # Use default if nothing input.
+  [ -z "${the_choice}" ] \
+    && the_choice="${default_choice}"
+
+  # Lowercase the input.
+  the_choice="${the_choice,,}"
+
+  # ***
 
   # We could consider just the first character:
   #   the_choice=${the_choice:0:1}
-  # or just demand a one-character response.
+  # Or we could allow full and partial words (y, ye, yes, etc.).
+  # But we don't. We require a one-character response.
   if [ "${the_choice}" != "y" ] && [ "${the_choice}" != "s" ]; then
+    # Any input other than 'Y', 'y', 'S', or 's'.
     return 1
   fi
+
+  # Prints either 'y' or 's'.
   printf %s "${the_choice}"
 }
 
